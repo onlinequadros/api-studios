0b8516d45e381ba6e2fa8ff8ed7673ac8a16a23a
jeandson <jeandsontb@gmail.com>
2023-09-04 12:23:47 -0300

    Fix: notification payment

--- src/modules/tenant/paymentGetNetNotification/payment-getnet-notification.service.ts
+++ src/modules/tenant/paymentGetNetNotification/payment-getnet-notification.service.ts
@@ -23,15 +23,15 @@ export class PaymentGetNetNotificationService {
     receiver_cpf,
     terminal_nsu,
   }: IPaymentPixParams) {
-    return { ok: true };
-    // const responsePixNotification = await lastValueFrom(
-    //   this.httpService
-    //     .get(
-    //       `${process.env.HOST_SERVER}/getnet-notification/pix?payment_type=${payment_type}&customer_id=${customer_id}&order_id=${order_id}&payment_id=${payment_id}&amount=${amount}&status=${status}&transaction_id=${transaction_id}&transaction_timestamp=${transaction_timestamp}&receiver_psp_name=${receiver_psp_name}&receiver_psp_code=${receiver_psp_code}&receiver_name=${receiver_name}&receiver_cnpj=${receiver_cnpj}&receiver_cpf=${receiver_cpf}&terminal_nsu=${terminal_nsu}`,
-    //     )
-    //     .pipe(map((response) => response.data)),
-    // );
+    // return { ok: true };
+    const responsePixNotification = await lastValueFrom(
+      this.httpService
+        .get(
+          `${process.env.HOST_SERVER}?payment_type=${payment_type}&customer_id=${customer_id}&order_id=${order_id}&payment_id=${payment_id}&amount=${amount}&status=${status}&transaction_id=${transaction_id}&transaction_timestamp=${transaction_timestamp}&receiver_psp_name=${receiver_psp_name}&receiver_psp_code=${receiver_psp_code}&receiver_name=${receiver_name}&receiver_cnpj=${receiver_cnpj}&receiver_cpf=${receiver_cpf}&terminal_nsu=${terminal_nsu}`,
+        )
+        .pipe(map((response) => response.data)),
+    );
 
-    // console.log('response Pix ', responsePixNotification);
+    console.log('response Pix ', responsePixNotification);
   }
 }
