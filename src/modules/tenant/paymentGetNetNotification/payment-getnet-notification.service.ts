58712302f4c87b9e086a9680d27b56bb76ed7114
jeandson <jeandsontb@gmail.com>
2023-09-21 12:57:24 -0300

    Fix notification pix

--- src/modules/tenant/paymentGetNetNotification/payment-getnet-notification.service.ts
+++ src/modules/tenant/paymentGetNetNotification/payment-getnet-notification.service.ts
@@ -23,18 +23,19 @@ export class PaymentGetNetNotificationService {
     receiver_cpf,
     terminal_nsu,
   }: IPaymentPixParams) {
-    // return { ok: true };
-    console.log('01');
     const responsePixNotification = await lastValueFrom(
       this.httpService
         .get(
           `${process.env.HOST_SERVER}?payment_type=${payment_type}&customer_id=${customer_id}&order_id=${order_id}&payment_id=${payment_id}&amount=${amount}&status=${status}&transaction_id=${transaction_id}&transaction_timestamp=${transaction_timestamp}&receiver_psp_name=${receiver_psp_name}&receiver_psp_code=${receiver_psp_code}&receiver_name=${receiver_name}&receiver_cnpj=${receiver_cnpj}&receiver_cpf=${receiver_cpf}&terminal_nsu=${terminal_nsu}`,
         )
-        .pipe(map((response) => response.data)),
+        .pipe(
+          map((response) => {
+            console.log('response ', response);
+            return response.data;
+          }),
+        ),
     );
 
-    console.log('02');
-    console.log('response Pix ', responsePixNotification);
     return responsePixNotification;
   }
 }
